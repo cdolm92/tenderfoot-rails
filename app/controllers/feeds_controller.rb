d7acdee131c7303bcc4236df5a9755668046c63e
carobinson <carly.a.robinson@gmail.com>
2015-04-26 09:38:09 -0400

    initial commit

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,24 @@
+class FeedsController < ApplicationController
+	def welcome
+	end
+	
+	def index
+	end
+
+	def show
+	end
+
+	def new 
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def edit
+	end
+
+	
+end
